189c54e57d7da070c48ce04322fc2829a8c1977d
eduardotr2810 <eduardotr2810mail.com>
2025-03-29 01:46:37 -0500

    subida de archivos iniciales

--- obj/Debug/net8.0/zapat.AssemblyInfo.cs
+++ obj/Debug/net8.0/zapat.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zapat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd95c45d8040b9f79768d41a1b31824b240fb89e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7335ab3e3a6a1b8fbdab979d2b64930a2e1f3ceb")]
 [assembly: System.Reflection.AssemblyProductAttribute("zapat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zapat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
