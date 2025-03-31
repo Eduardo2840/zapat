e480648ee74d881c9804aeb7969ef51a93c2e07b
Keps <kevinpasionpayano@gmail.com>
2025-03-30 23:28:55 -0500

    avance5Cata

--- obj/Debug/net8.0/zapat.AssemblyInfo.cs
+++ obj/Debug/net8.0/zapat.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zapat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+550dc35ee2afd178e83670e23721fa3b34038e6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69f2a1fa472821062042641f637b10601142bdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("zapat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zapat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
