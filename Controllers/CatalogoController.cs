a44d54d9d701b30be352305d7c1a7044f5cfc9da
DaysGone <kevinpasionpayano@gmail.com>
2025-04-15 20:26:30 -0500

    carritocambio

--- Controllers/CatalogoController.cs
+++ Controllers/CatalogoController.cs
@@ -5,22 +5,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using zapat.Data;
+using zapat.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace zapat.Controllers
 {
     
+    
     public class CatalogoController : Controller
     {
         private readonly ILogger<CatalogoController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public CatalogoController(ILogger<CatalogoController> logger)
+        public CatalogoController(ILogger<CatalogoController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            // Aquí puedes agregar la lógica para obtener el producto por su ID
+            // y pasarlo a la vista.
+
+            Producto objProduct = await _context.DbSetProducto.FindAsync(id);
+            if (objProduct == null)
+            {
+                return NotFound();
+            }
+            return View(objProduct);
+           
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
